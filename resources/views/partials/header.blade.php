93fcfc9b7aa69eaf986775d6cfe1bf2b63744a5c
RoccoCerro <rcerro02@gmail.com>
2024-05-29 14:58:24 +0200

    Add: Comic controller

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+  <div class="container">
+    <div class="row">
+      <div class="col"></div>
+      <div class="col">
+        <ul>
+          <li><a href="{{route('comics')}}">COMICS</a></li>
+          <li><a href="{{route('home')}}">HOME</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</header>
